bf302a424a8721325d4dd602c0888f229fc56900
Oliver0804 <icetzsr@gmail.com>
2023-09-25 00:03:27 +0800

    更新json

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function showResult() {
 
     console.log("開始計算分數...");
 
-    fetch('questions.json')
+    fetch('https://raw.githubusercontent.com/Oliver0804/UAV_Choicer/main/questions.json')
         .then(response => response.json())
         .then(questions => {
             questions.forEach((question, index) => {
@@ -53,4 +53,40 @@ function showResult() {
         .catch(error => {
             console.error("讀取問題出錯:", error);
         });
+}
+
+
+var myChart; // 宣告一個全局變數來保存圖表實例
+
+function renderChart(results) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    
+    // 檢查是否已經有一個圖表存在
+    if (myChart) {
+        myChart.destroy(); // 如果存在，就銷毀它
+    }
+
+    // 然後創建新的圖表
+    myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['A類', 'B類', 'C類', 'D類'],
+            datasets: [{
+                data: [results.A, results.B, results.C, results.D],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+    });
 }
