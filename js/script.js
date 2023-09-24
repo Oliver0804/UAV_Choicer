41199aa1f4db2e44c19038b8b34cce2ed2fb1d9f
Oliver0804 <icetzsr@gmail.com>
2023-09-24 23:59:07 +0800

    增加題目權重

--- js/script.js
+++ js/script.js
@@ -5,73 +5,52 @@ function showResult() {
     let scoreC = 0;
     let scoreD = 0;
 
-    // 使用querySelectorAll來選取所有符合名稱前綴的輸入元素
-    let totalQuestions = document.querySelectorAll('input[name^="question"]').length / 4; // 因為每個問題有4個選項
+    console.log("開始計算分數...");
 
-    // 對所有問題進行迴圈
-    for (let i = 1; i <= totalQuestions; i++) {
-        let selectedValue = document.querySelector(`input[name="question${i}"]:checked`).value;
+    fetch('questions.json')
+        .then(response => response.json())
+        .then(questions => {
+            questions.forEach((question, index) => {
+                let selectedValue = document.querySelector(`input[name="question${index + 1}"]:checked`).value;
 
-        switch (selectedValue) {
-            case 'A':
-                scoreA += 1;
-                break;
-            case 'B':
-                scoreB += 1;
-                break;
-            case 'C':
-                scoreC += 1;
-                break;
-            case 'D':
-                scoreD += 1;
-                break;
-        }
-    }
+                console.log(`問題${index + 1}選擇的答案是: ${selectedValue}`);
 
-    let results = {
-        A: scoreA,
-        B: scoreB,
-        C: scoreC,
-        D: scoreD
-    };
+                // 針對選擇的選項增加相應的分數
+                switch (selectedValue) {
+                    case 'A':
+                        scoreA += question.weight;
+                        console.log(`A分數增加${question.weight}，目前A分數是：${scoreA}`);
+                        break;
+                    case 'B':
+                        scoreB += question.weight;
+                        console.log(`B分數增加${question.weight}，目前B分數是：${scoreB}`);
+                        break;
+                    case 'C':
+                        scoreC += question.weight;
+                        console.log(`C分數增加${question.weight}，目前C分數是：${scoreC}`);
+                        break;
+                    case 'D':
+                        scoreD += question.weight;
+                        console.log(`D分數增加${question.weight}，目前D分數是：${scoreD}`);
+                        break;
+                }
+            });
 
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `A類分數：${results.A}，B類分數：${results.B}，C類分數：${results.C}，D類分數：${results.D}`;
+            let results = {
+                A: scoreA,
+                B: scoreB,
+                C: scoreC,
+                D: scoreD
+            };
 
-    renderChart(results);
-}
+            var resultDiv = document.getElementById('result');
+            resultDiv.innerHTML = `A類分數：${results.A}，B類分數：${results.B}，C類分數：${results.C}，D類分數：${results.D}`;
 
-var myChart; // 宣告一個全局變數來保存圖表實例
+            console.log("計算完成，結果已顯示在頁面上");
 
-function renderChart(results) {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    
-    // 檢查是否已經有一個圖表存在
-    if (myChart) {
-        myChart.destroy(); // 如果存在，就銷毀它
-    }
-
-    // 然後創建新的圖表
-    myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['A類', 'B類', 'C類', 'D類'],
-            datasets: [{
-                data: [results.A, results.B, results.C, results.D],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-    });
+            renderChart(results);
+        })
+        .catch(error => {
+            console.error("讀取問題出錯:", error);
+        });
 }
